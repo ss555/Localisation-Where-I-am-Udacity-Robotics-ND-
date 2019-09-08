2a72d67958aec226b333367086f7e0046576df2f
ISRAILOV SARDOR <5sardorisrailov5@gmail.com>
2019-09-08 13:36:38 +0200

    catkin_ws for udacity_bot sardor
    f

---
+++ catkin_ws/build/muth_bot/CMakeFiles/goto_goal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goto_goal.dir/src/goto_goal.cpp.o"
+  "/home/workspace/catkin_ws/devel/lib/muth_bot/goto_goal.pdb"
+  "/home/workspace/catkin_ws/devel/lib/muth_bot/goto_goal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goto_goal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/sardor/CMakeFiles/navigation_goalS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/navigation_goalS.dir/src/navigation_goalS.cpp.o"
+  "/home/workspace/catkin_ws/devel/lib/sardor/navigation_goalS.pdb"
+  "/home/workspace/catkin_ws/devel/lib/sardor/navigation_goalS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/navigation_goalS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
